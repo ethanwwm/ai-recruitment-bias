503d4131ffe192b4c16a673f23fc0535f78d1781
Ethan Wong <33324347+ethanwwm@users.noreply.github.com>
2019-02-10 11:46:45 +0000

    Create graphs.py

---
+++ graphs.py
@@ -0,0 +1,31 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('data/hiring-patterns.csv')
+
+df['Hired'].replace('NO', 'No', inplace=True)
+df['Gender'].replace('Female ', 'Female', inplace=True)
+df['Gender'].replace(' N/A', 'N/A', inplace=True)
+df['Gender'].replace('   N/A', 'N/A', inplace=True)
+
+trace1 = go.Bar(
+    x=['White','BME'],
+    y=[white.shape[0], bme.shape[0]],
+    name='Applied'
+)
+trace2 = go.Bar(
+    x=['White','BME'],
+    y=[white[white.Interviewed == 'Yes'].shape[0], bme[bme.Interviewed == 'Yes'].shape[0]],
+    name='Interviewed'
+)
+trace3 = go.Bar(
+    x=['White','BME'],
+    y=[white[white.Hired == 'Yes'].shape[0], bme[bme.Hired == 'Yes'].shape[0]],
+    name='Hired'
+)
+
+data = [trace1, trace2, trace3]
+layout = go.Layout(
+    barmode='group'
+)
